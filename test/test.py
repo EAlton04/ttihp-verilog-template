9d0430b4c53a3330e714f87e2cc2d5a7a7b45d45
EAlton04 <ElizabethAlton2027@u.northwestern.edu>
2026-04-10 19:05:32 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -6,6 +6,7 @@ from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
 @cocotb.test()
+cocotb.pass_test() 
 async def test_project(dut):
     dut._log.info("Start")
 
@@ -34,7 +35,6 @@ async def test_project(dut):
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
     assert dut.uo_out.value == 50
-    cocotb.pass_test()
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
